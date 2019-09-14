b4c711bbc643a63de9332902ec15e54092a2d205
Tamas Kakuszi <tamas.kakuszi@gmail.com>
2019-09-14 18:40:12 +0200

    removed dummy hello

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -6,13 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class BackendApplication {
-
-	@GetMapping("/hello")
-	public String sayHello() {
-		return "Hello";
-	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
